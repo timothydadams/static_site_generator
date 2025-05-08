4b310201c99063e0a295e1d621cd0226614d595b
Timothy Adams <adams.timothy.d@gmail.com>
2025-05-08 14:51:09 -0400

    add __repr__ for leafNode easy printing and troubleshooting

--- src/leafNode.py
+++ src/leafNode.py
@@ -4,11 +4,13 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
     def to_html(self):
         has_props = self.props != None
         if self.value==None:
             raise ValueError("leaf nodes must have a value")
         if self.tag==None:
             return f"{self.value}"
-        
         return f"<{self.tag}{self.props_to_html() if has_props else ''}>{self.value}</{self.tag}>"

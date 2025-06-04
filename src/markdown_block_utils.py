13d71ae4340ff5e4cd233c88fce1afac4fe08486
Timothy Adams <adams.timothy.d@gmail.com>
2025-06-04 13:02:50 -0400

    add block type determination and tests

--- src/markdown_block_utils.py
+++ src/markdown_block_utils.py
@@ -1,4 +1,35 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UL = "unordered_list"
+    OL = "ordered_list"
+
+def check_for_ordered_list(line):
+    if line[1].startswith(f"{line[0] + 1}. "):
+        return True
+    return False
+
+def block_to_block_type(block):
+    heading_prefixes = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+    lines = block.splitlines()
+
+    if block.startswith(tuple(heading_prefixes)):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif all(map(lambda x: x.startswith(">"), lines )):
+        return BlockType.QUOTE
+    elif all(map(lambda x: x.startswith("- "), lines )):
+        return BlockType.UL
+    elif all(map(check_for_ordered_list, enumerate(lines) )):
+        return BlockType.OL
+    else:
+        return BlockType.PARAGRAPH
 
 def normalize_endings(text):
     return text.replace("\r\n", "\n").replace("\r", "\n")
@@ -9,3 +40,4 @@ def markdown_to_blocks(markdown):
     return list(map(
         lambda x : x.strip(), blocks
     ))
+
--- src/test_markdown_block_utils.py
+++ src/test_markdown_block_utils.py
@@ -1,8 +1,9 @@
 import unittest
 from markdown_block_utils import (
     markdown_to_blocks,
+    block_to_block_type,
+    BlockType,
 )
-from textNode import TextNode, TextType
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -47,6 +48,47 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+class TestBlockType(unittest.TestCase):
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UL)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OL)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_blocks_to_block_type(self):
+        blocks = [
+                "# This is **bolded** paragraph",
+                "## This is another paragraph",
+                "- This is a list\n- with items",
+                ">Just another day in the\n>office",
+                "1. line one\n2. line two\n3. line three\n4. line four",
+                "3. wrong ordered\n5. test",
+                "```i am code```",
+        ]
+        block_types = list(map(block_to_block_type, blocks))
+        self.assertEqual(
+            block_types,
+            [
+                BlockType.HEADING,
+                BlockType.HEADING,
+                BlockType.UL,
+                BlockType.QUOTE,
+                BlockType.OL,
+                BlockType.PARAGRAPH,
+                BlockType.CODE
+            ],
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
